429acdd61f5724189fd5df44230af8d36f867411
ovarz <ovarz_03@yahoo.com>
2023-08-23 18:49:03 +0700

    update

--- home.php
+++ home.php
@@ -18,7 +18,7 @@
   <span class="width-max">
     <h1 class="shi-title">Impavida</h1>
     <h2 class="shi-subtitle">We are ready to help with <span>Extraordinary</span> options for your <span>legal solutions</span></h2>
-	<div class="shi-image content_center">
+	<div class="shi-image">
 	  <div class="shi-image-container flex_ori thumb-loading">
 	    <img alt="Impavida Founder" class="lazyload" data-original="img/intro-trio-small.png"
 		  data-srcset="img/intro-trio-small.png 567w,
@@ -26,29 +26,32 @@
 		  img/intro-trio-large.png 1920w"
 		/>
 	  </div>
+	  <div class="shi-prime">
+		<?php 
+		  $prime_array = array();
+		  $prime_array[]=array('prime_icon'=>'reputable','prime_label'=>'Reputable');
+		  $prime_array[]=array('prime_icon'=>'commited','prime_label'=>'Commited');
+		  $prime_array[]=array('prime_icon'=>'solving','prime_label'=>'Problem Solving');
+		  $prime_array[]=array('prime_icon'=>'effective','prime_label'=>'Cost Effective');
+		  foreach($prime_array as $prime_list){
+		?>
+		  <div class="shi-prime-box">
+			<div class="shi-prime-container">
+			  <?php require ($_SERVER['IL'].'img/icon/prime-'. $prime_list['prime_icon'] .'.svg')?>
+			  <h3 class="shi-prime-label"><?php echo($prime_list['prime_label'])?></h3>
+			</div>
+		  </div>
+		<?php } ?>
+	  </div>
 	</div>
   </span>
 </section>
 
 
 
-<section aria-label="Prime" class="section-container section-dark section-home-prime">
+<section aria-label="Prime" class="section-container section-home-about">
   <span class="width-max">
-    <?php 
-      $prime_array = array();
-      $prime_array[]=array('prime_icon'=>'reputable','prime_label'=>'Reputable');
-      $prime_array[]=array('prime_icon'=>'commited','prime_label'=>'Commited');
-      $prime_array[]=array('prime_icon'=>'solving','prime_label'=>'Problem Solving');
-      $prime_array[]=array('prime_icon'=>'effective','prime_label'=>'Cost Effective');
-      foreach($prime_array as $prime_list){
-    ?>
-	  <div class="shp-box">
-	    <div class="shp-container">
-		  <?php require ($_SERVER['IL'].'img/icon/prime-'. $prime_list['prime_icon'] .'.svg')?>
-		  <h3 class="shp-label"><?php echo($prime_list['prime_label'])?></h3>
-		</div>
-	  </div>
-    <?php } ?>
+    tes
   </span>
 </section>
 
