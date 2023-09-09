e0388826e2538b80dd8462a56421d3d4a86f951c
ovarz <ovarz_03@yahoo.com>
2023-09-09 08:05:15 +0700

    email

--- module/profile-box.php
+++ module/profile-box.php
@@ -5,7 +5,6 @@
   <h2 class="profile-name"><?php echo $page_title; ?></h2>
   <h3 class="profile-title"><?php echo $profile_title; ?></h3>
   <a aria-label="Email <?php echo $page_title; ?>" alt="Email <?php echo $page_title; ?>" class="btn profile-email" href="mailto:<?php echo $profile_email; ?>">
-    <?php require ($_SERVER['IL'].'img/icon/footer-mail.svg')?>
-	<span class="profile-email-label">Contact Me</span>
+	<span class="profile-email-label"><?php echo $profile_email; ?></span>
   </a>
 </div>
